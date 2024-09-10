cafc27aa2b5b5be10adca76a3b9668e67a660c37
stefkn <stefan.nowak@tails.com>
2024-09-10 18:15:15 +0100

    init main loop

--- genetic-tac-toe/src/main.rs
+++ genetic-tac-toe/src/main.rs
@@ -1,6 +1,16 @@
 mod tic_tac_toe;
 mod player;
+mod genetic_algorithm;
+
+use genetic_algorithm::GeneticAlgorithm;
 
 fn main() {
-    println!("Hello, world!");
+    println!("===============genetic-tac-toe=================");
+    let mut ga = GeneticAlgorithm::new(
+        100, 
+        0.01, 
+        0.7, 
+        50
+    );
+    ga.evolve();
 }
